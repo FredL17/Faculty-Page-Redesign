de2cc6d6a64794ea53adb06b0653183a67f28640
DanaShkokani <shkokani.1@osu.edu>
2019-10-10 18:24:42 -0400

    added a ruby file that generates the names as a template

---
+++ helper.rb
@@ -0,0 +1,10 @@
+
+names = ["Jiayuan Wang, PhD",
+"Tianqi Li, PhD",
+"Sayan Mandal, PhD",
+"Cheng Xin, PhD",
+"Ryan Slechta, PhD",
+"Tao Hou, PhD",
+"Soham Mukherjee, PhD"]
+names.sort!
+names.each {|name| puts "<li class=\"names\">#{name}</li>\n"}
