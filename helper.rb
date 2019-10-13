693da99e93a64289f509f93f5583aefc7590e73d
DanaShkokani <shkokani.1@osu.edu>
2019-10-13 14:37:42 -0400

    research pages

--- helper.rb
+++ helper.rb
@@ -1,10 +1,8 @@
 
-names = ["Jiayuan Wang, PhD",
-"Tianqi Li, PhD",
-"Sayan Mandal, PhD",
-"Cheng Xin, PhD",
-"Ryan Slechta, PhD",
-"Tao Hou, PhD",
-"Soham Mukherjee, PhD"]
+names = ["Cocone: Software for surface reconstruction based on paper.",
+"AMLS: software for noisy point cloud smoothing based on paper.",
+"SingularCocone: software for reconstructing surfaces with singular features",
+"Peel: software for reconstructing surfaces with boundaries based on paper", "Gathan: software for curve reconstruction based on paper.",
+"Nearest Neighbor: curve reconstruction Applet based on paper and a topology clean up."]
 names.sort!
-names.each {|name| puts "<li class=\"names\">#{name}</li>\n"}
+names.each {|name| puts "\t\t\t\t<li><a href =\"\">#{name}</a></li>\n"}
