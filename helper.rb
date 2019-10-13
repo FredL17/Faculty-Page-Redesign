39efea0701add956ce5f9ef2354456f2ff2200cb
DanaShkokani <41482126+DanaShkokani@users.noreply.github.com>
2019-10-13 19:49:15 -0400

    Delete helper.rb

--- helper.rb
+++
@@ -1,8 +0,0 @@
-
-names = ["Cocone: Software for surface reconstruction based on paper.",
-"AMLS: software for noisy point cloud smoothing based on paper.",
-"SingularCocone: software for reconstructing surfaces with singular features",
-"Peel: software for reconstructing surfaces with boundaries based on paper", "Gathan: software for curve reconstruction based on paper.",
-"Nearest Neighbor: curve reconstruction Applet based on paper and a topology clean up."]
-names.sort!
-names.each {|name| puts "\t\t\t\t<li><a href =\"\">#{name}</a></li>\n"}
